a5f8d938256b3073a7267497ef08138f13e88bfc
Luciano Oliveira and Safiy Cham <luciano+safiy@>
2017-10-17 14:30:28 -0700

    add pokemon API

--- build/js/app.js
+++ build/js/app.js
@@ -94,4 +94,44 @@ $(document).ready(function () {
   });
 });
 
+$(document).ready(function () {
+
+  // $('#pokemon-form').submit(function(e){
+  //   e.preventDefault();
+  //   pokemonResult = $('.pokemon-id').val();
+  //   console.log(pokemonResult);
+  //   pokeSubmit();
+  // });
+
+
+  // function pokeSubmit(){
+  //   var param = document.getElementById("pokeInput").value;
+  //   var pokeURL = "http://pokeapi.co/api/v1/pokemon/" + param;
+  //
+  //   $.getJSON(pokeURL, function(data){
+  //     console.log(data);
+  //     console.log(JSON.stringify(data, null, "  "));
+  //     console.log(param);
+  //   });
+  // }
+
+  $("#my-button").click(function () {
+
+    var myInput = $("#my-input").val();
+    console.log(myInput);
+    var myUrl = "https://pokeapi.co/api/v2/pokemon/" + myInput + "/";
+    console.log(myUrl);
+
+    $.ajax({
+      method: "GET",
+      url: myUrl,
+      success: function success(data) {
+        $("#poke-name").html(data.id);
+
+        $("#poke-img").attr("src", data.sprites.front_default);
+      }
+    });
+  });
+});
+
 },{"./../js/animal.js":1}]},{},[2]);
